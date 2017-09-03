c6cba683fc7e7d27c5e4146f584077a4fd97cac7
Henry Paulino <henrypl360@gmail.com>
2017-09-02 23:10:57 -0400

    add logout button

--- src/components/Settings/index.js
+++ src/components/Settings/index.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, AsyncStorage } from 'react-native'
+import { Actions } from 'react-native-router-flux'
 
 class Settings extends Component {
   render() {
     return (
       <View>
         <Text>Settings</Text>
+        <TouchableOpacity onPress={() => {
+          AsyncStorage.removeItem('UserSession', (error) => {
+            if (!error) {
+              Actions.login()
+            }
+          })
+        }}>
+          <Text>Logout</Text>
+        </TouchableOpacity>
       </View>
     )
   }
